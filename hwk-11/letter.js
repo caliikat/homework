964469010628ee1ae3257c32d12c364839e036d2
caliikat <katvonallen@gmail.com>
2017-12-07 15:34:30 -0600

    update

--- hwk-11/letter.js
+++ hwk-11/letter.js
@@ -1,18 +1,17 @@
-this homework was very hard for me. i received tutoring on what i found on the net... and i am not sure i would be able to do this solo just yet. 
-i wish more time was spent on homework assignments to not feel like total failure with complicated javascript. maybe that is just me. 
 
 
-// function Letter(let) {
-//   this.charac = let;
-//   this.appear = false;
 
-//   this.letterRender = function() {
-//     if (this.appear === false) {
-//       return "__";
-//     } else {
-//       return this.charac;
-//     }
-//   }
-// };
+ function Letter(let) {
+   this.charac = let;
+   this.appear = false;
 
-// module.exports = Letter;
+   this.letterRender = function() {
+     if (this.appear === false) {
+       return "__";
+     } else {
+       return this.charac;
+     }
+   }
+ };
+
+ module.exports = Letter;
--- hwk-11/main.js
+++ hwk-11/main.js
@@ -1,46 +1,43 @@
-this homework was very hard for me. i received tutoring on what i found on the net... and i am not sure i would be able to do this solo just yet. 
-i wish more time was spent on homework assignments to not feel like total failure with complicated javascript. maybe that is just me. 
+
+var prompt = require("prompt");
+prompt.start();
+var Word = require("./word.js");
 
 
-// var prompt = require("prompt");
-// prompt.start();
-// var Word = require("./word.js");
+var game = {
+  wordBank: ["rachel", "joey", "chandler", "monica", "phoebe", "ugly naked guy", "triplets", "ross", "coffee shop"],
+  guessesRemaining: 10,
+  currentWrd: null,
 
+  startGame: function(wrd) {
+    var newWord = new Word(this.wordBank[Math.floor(Math.random()*this.wordBank.length)]);
+    this.currentWrd = newWord;
+    this.currentWrd.getLets();
+    this.keepPromptingUser();
+  },
+  keepPromptingUser: function() {
 
-// var game = {
-//   wordBank: ["rachel", "joey", "chandler", "monica", "phoebe", "ugly naked guy", "triplets", "ross", "coffee shop"],
-//   guessesRemaining: 10,
-//   currentWrd: null,
-
-//   startGame: function(wrd) {
-//     var newWord = new Word(this.wordBank[Math.floor(Math.random()*this.wordBank.length)]);
-//     this.currentWrd = newWord;
-//     this.currentWrd.getLets();
-//     this.keepPromptingUser();
-//   },
-//   keepPromptingUser: function() {
-
-//     var self = this;
+    var self = this;
     
 
-//     prompt.get(["guessLetter"], function(err, result) {
-//       console.log("The Letter or space you guessed is " + result.guessLetter);
-//       var findHowManyOfUserGuesses = self.currentWrd.checkIfLetterFound(result.guessLetter);
+    prompt.get(["guessLetter"], function(err, result) {
+      console.log("The Letter or space you guessed is " + result.guessLetter);
+      var findHowManyOfUserGuesses = self.currentWrd.checkIfLetterFound(result.guessLetter);
 
-//       if (findHowManyOfUserGuesses === 0) {
-//         console.log("You guessed wrong!");
-//         self.guessesRemaining -= 1;
-//       } else {
-//         console.log("You guessed right!");
-//         if (self.currentWrd.didWeFindTheWord() === true) {
-//           console.log("You Won!!!");
-//           return 1;
-//         } else {
-//           console.log("guesses remaining: " + self.guessesRemaining);
-//           console.log(self.currentWrd.wordRender());
-//         }
-//       }
-//       if (self.guessesRemaining > 0 && self.currentWrd.found === false) {
+      if (findHowManyOfUserGuesses === 0) {
+        console.log("You guessed wrong!");
+        self.guessesRemaining -= 1;
+      } else {
+        console.log("You guessed right!");
+        if (self.currentWrd.didWeFindTheWord() === true) {
+          console.log("You Won!!!");
+          return 1;
+        } else {
+          console.log("guesses remaining: " + self.guessesRemaining);
+          console.log(self.currentWrd.wordRender());
+        }
+      }
+      if (self.guessesRemaining > 0 && self.currentWrd.found === false) {
         self.keepPromptingUser();
       } else if (self.guessesRemaining === 0) {
         console.log("this game is done");
